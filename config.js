715085bf7c3e0206c36927b26781fe521c92747d
ZavierDev <riwanzavier45@gmail.com>
2026-01-29 14:50:30 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,76 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+      
+global.pairingNumber = 62881026602593
+
+global.owner = [
+  ['62881026602593', 'Zavier', true]
+]
+global.mods = []
+
+global.namebot = "Zavier"
+global.author = 'Zavier'
+
+global.wait = 'bentar'
+global.eror = 'error cik😹'
+
+global.saluran = 'link ch lu'
+global.idch = 'id ch lu'
+
+global.stickpack = 'Croted By'
+global.stickauth = namebot
+
+global.multiplier = 38 // The higher, The harder levelup
+
+/*============== EMOJI ==============*/
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      level: '📊',
+      limit: '🎫',
+      health: '❤️',
+      stamina: '🔋',
+      exp: '✨',
+      money: '💹',
+      bank: '🏦',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🛍️',
+      mythic: '🎁',
+      legendary: '🗃️',
+      superior: '💼',
+      pet: '🔖',
+      trash: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      pickaxe: '⛏️',
+      fishingrod: '🎣',
+      wood: '🪵',
+      rock: '🪨',
+      string: '🕸️',
+      horse: '🐴',
+      cat: '🐱',
+      dog: '🐶',
+      fox: '🦊',
+      petFood: '🍖',
+      iron: '⛓️',
+      gold: '🪙',
+      emerald: '❇️',
+      upgrader: '🧰'
+      
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
